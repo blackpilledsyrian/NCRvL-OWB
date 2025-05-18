d8ab878fea36b282421c87836bae24e22ed4f64c
blackpilledsyrian <hamooozmugharbel@gmail.com>
2025-05-18 20:12:21 +0300

    Fixes
    Fixed infinite spawn ins for CES

--- common/defines/01_defines.lua
+++ common/defines/01_defines.lua
@@ -450,12 +450,15 @@ NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0 	--Base cost to unlock a 
 NDefines.NFocus.FOCUS_POINT_DAYS = 1
 
 
+
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 168 --Makes the game much more bearable in MP, This is actually in hours, not days, making it 7 days worth.
 
 
 --beyond these are change to NCRvLegion submod for OWB, also Meme need to never touch a LUA file ever again
 NDefines.NCountry.BASE_MAX_COMMAND_POWER = 200.0
 NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_VALUE = 0 -- The minimum number of factories we have to put on consumer goods, by value.		
+NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT = 0.0	-- The minimum number of factories we have to put on consumer goods, in percent.
+
 NDefines.NTechnology.BASE_TECH_COST = 100 				-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
 
 NDefines.NPolitics.ARMY_LEADER_MAX_COST = 100				-- max cost BEFORE modifiers
@@ -464,8 +467,12 @@ NDefines.NPolitics.NAVY_LEADER_MAX_COST = 100				-- max cost BEFORE modifiers
 NDefines.NBuildings.OWNER_CHANGE_EXTRA_SHARED_SLOTS_FACTOR = 0.5 --Scale factor of extra shared slots when state owner change.
 
 NDefines.NMilitary.RELIABILTY_RECOVERY = 0.1                    -- factor affecting how much equipment is returned "from the dead"
+NDefines.NMilitary.PLANNING_MAX = 0.05                           	-- can get more from techs
+
 NDefines.NAir.BOMBING_TARGETING_RANDOM_FACTOR = 0.25							-- % of picking the wrong target
 NDefines.NAir.BOMBING_PROV_BUILD_PRIO_SCALE = 1.5							-- Scale of the selected priority for provincial buildings
+NDefines.NAir.BOMBING_STATE_BUILD_PRIO_SCALE = 1.5							-- Scale of the selected priority for state buildings
+NDefines.NAir.BOMBING_INFRA_PRIO_SCALE = 0.7									-- Scale of the selected priority for infastryctyre
 
 NDefines.NNavy.UNDERWAY_REPLENISHMENT_CONVOY_COST_PER_FUEL = 0.35	-- Cost in convoys for underway replenishment multiplied by max daily fuel consumption (rounded up)
 
